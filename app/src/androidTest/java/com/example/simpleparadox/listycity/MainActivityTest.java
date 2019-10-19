55aec6afe9637bf87b4dcec13232d6ff1675787b
jakaria08 <jakaria.rabbi@live.com>
2019-10-19 01:38:22 -0600

    Demo...

--- app/src/androidTest/java/com/example/simpleparadox/listycity/MainActivityTest.java
+++ app/src/androidTest/java/com/example/simpleparadox/listycity/MainActivityTest.java
@@ -1,5 +1,14 @@
 package com.example.simpleparadox.listycity;
-import org.junit.j
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
 }
