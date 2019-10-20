e5c7396f798d4a1f43b76c89fe45c2aefb8ea557
jakaria08 <jakaria.rabbi@live.com>
2019-10-19 22:15:12 -0600

    Demo final..

--- app/src/androidTest/java/com/example/simpleparadox/listycity/MainActivityTest.java
+++ app/src/androidTest/java/com/example/simpleparadox/listycity/MainActivityTest.java
@@ -21,13 +21,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest extends ActivityTestRule<MainActivity> {
+public class MainActivityTest{
 
     private Solo solo;
-
-    public MainActivityTest(){
-        super(MainActivity.class, false, true);
-    }
 
     @Rule
     public ActivityTestRule<MainActivity> rule =
